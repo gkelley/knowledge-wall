bc87db33dd1f66fc5ec8b757f993d82bd0fcb34a
GKelley <GKelley@GKELLEY-0702201>
2019-05-16 13:36:32 -0500

    Removed Gradient TEmp

--- knowledge-wall/src/pages/Kiosk/KioskHome/styles.ts
+++ knowledge-wall/src/pages/Kiosk/KioskHome/styles.ts
@@ -1,12 +1,12 @@
 
 const styles = {
     button: {
-        background: 'linear-gradient(45deg, #ace9db 30%, #7488fc 90%)',
+        // background: 'linear-gradient(45deg, #ace9db 30%, #7488fc 90%)',
         borderRadius: 3,
         border: 0,
-        color: 'white',
+        // color: 'white',
         padding: '0 30px',
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        boxShadow: '0 3px 5px 2px #c1ccc9',
         fontSize: "3vw"
     }, 
     gridContainer: {
